2ad73e5cb38529a901e6ab63a26cdc42b12884a8
Doug Meeker <wog400@hotmail.com>
2018-09-04 22:15:52 -0700

    Add missing file.

---
+++ RangeVote.Server/Controllers/VoterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RangeVote.Server.Data;
+using RangeVote.Common;
+using System;
+
+namespace RangeVote.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoterController : ControllerBase
+    {
+        private readonly IRepository _repository;
+
+        public VoterController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET api/Voter
+        [HttpGet]
+        public Int32 Get()
+        {
+            return _repository.GetVoters();
+        }
+    }
+}
+
+
